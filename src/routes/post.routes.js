3df5e2ecef8942a2aa973f120548b42e9a3020b1
Claudia.Alvaro <claudia.alvaro17@gmail.com>
2024-03-09 17:01:24 +0100

    FEAT: Delete post by id created

--- src/routes/post.routes.js
+++ src/routes/post.routes.js
@@ -1,12 +1,14 @@
 import { Router } from "express";
 import { auth } from "../middlewares/auth.js";
 import { isSuperAdmin } from "../middlewares/isSuperAdmin.js";
-import { createPost } from "../controllers/post.Controller.js";
+import { createPost, deletePostById } from "../controllers/post.Controller.js";
 
 
 const router = Router();
 
 router.post('/',auth,createPost)
+router.delete('/:id',auth, deletePostById)
+
 
 
 
