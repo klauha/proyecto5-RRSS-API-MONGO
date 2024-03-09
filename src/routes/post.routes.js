d221ea38e5f27bed0a77564463946d49bdd00084
klauha <claudiaalvaro17@gmail.com>
2024-03-09 18:35:25 +0100

    FEAT: Update post created

--- src/routes/post.routes.js
+++ src/routes/post.routes.js
@@ -1,13 +1,14 @@
 import { Router } from "express";
 import { auth } from "../middlewares/auth.js";
 import { isSuperAdmin } from "../middlewares/isSuperAdmin.js";
-import { createPost, deletePostById } from "../controllers/post.Controller.js";
+import { updatePost, createPost, deletePostById } from "../controllers/post.Controller.js";
 
 
 const router = Router();
 
 router.post('/',auth,createPost)
-router.delete('/:id',auth, deletePostById)
+router.delete('/:id',auth,deletePostById)
+router.put('/:id',auth,updatePost)
 
 
 
