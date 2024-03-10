3e6dfc44691f665dfe6782a51f89b6033130714e
Claudia.Alvaro <claudia.alvaro17@gmail.com>
2024-03-10 06:42:03 +0100

    FEAT: Get my Posts created

--- src/routes/post.routes.js
+++ src/routes/post.routes.js
@@ -1,7 +1,7 @@
 import { Router } from "express";
 import { auth } from "../middlewares/auth.js";
 import { isSuperAdmin } from "../middlewares/isSuperAdmin.js";
-import { updatePost, createPost, deletePostById } from "../controllers/post.Controller.js";
+import { updatePost, createPost, deletePostById, getMyPosts } from "../controllers/post.Controller.js";
 
 
 const router = Router();
@@ -9,6 +9,7 @@ const router = Router();
 router.post('/',auth,createPost)
 router.delete('/:id',auth,deletePostById)
 router.put('/:id',auth,updatePost)
+router.get ('/own', auth, getMyPosts)
 
 
 
